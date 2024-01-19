fc04152923727a33db28f8c9d60a0320c617d3eb
Pranjal Bandwal <85429823+PranjalBandwal@users.noreply.github.com>
2024-01-19 20:00:00 +0530

    Create control-statements.js

---
+++ control-statements.js
@@ -0,0 +1,11 @@
+// if statement---
+num=prompr("enter a number")
+if(num>=0){
+  console.log("Positive Number")
+}
+else if(num<0){
+  console.log("Negative Number")
+}
+else{
+  console.log("Enter a valid number!")
+}
