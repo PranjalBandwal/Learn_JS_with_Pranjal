30a2b0562e41b966302b91ebea12b199f7f1866e
Pranjal Bandwal <bandwal9@gmail.com>
2024-01-28 14:27:38 +0530

    forof

---
+++ basic_part2/iterations/for_of.js
@@ -0,0 +1,32 @@
+
+const arr=[1,2,3,4,5,6,7,8,9,10]
+
+for (const iterator of arr) {
+  // console.log(iterator);
+}
+
+const abs=["Hello","this","is","Javascript"]
+for (const a of abs) {
+  // console.log(a);
+}
+
+const str="Pranjal Bandwal"
+for (const i of str) {
+  if(i==" ")continue // space detect hui to skip krdega age wala iteration print hoga
+  // console.log(`each character ${i}`);
+}
+
+const map=new Map()
+map.set('name',"Pranjal")
+map.set(1,123)
+map.set('name',"Pranjal")// not include duplicates
+map.set('name',"Bandwal") // this will override the value
+// console.log(map.size);
+// console.log(map.get(1));
+// console.log(map.get('name'));
+
+for (const iterator of map) {
+  console.log(iterator);
+}
+map.delete(1)
+console.log(map.size);
