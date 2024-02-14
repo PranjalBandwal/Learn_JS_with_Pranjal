4b17223394fca2a23138b6d69519fcd633432204
Pranjal Bandwal <bandwal9@gmail.com>
2024-02-14 14:32:22 +0530

    with single button

--- projects/insta-friend/pranjal.js
+++ projects/insta-friend/pranjal.js
@@ -1,11 +1,23 @@
 const follow=document.querySelector("#follow")
-const unfollow=document.querySelector("#unfollow")
+// const unfollow=document.querySelector("#unfollow")
 const isstatus=document.querySelector("h5")
+ count=0
 follow.addEventListener("click",function(){
-    isstatus.innerHTML = "Following"
-    isstatus.style.color= "green"
+    if(count==0){
+        isstatus.innerHTML = "Following"
+        isstatus.style.color= "green"
+        follow.innerHTML ="Unfollow"
+        console.log("h")
+        count=1
+    }
+    else{
+        isstatus.innerHTML = "Follow Mountains"
+        isstatus.style.color= "Red"
+        follow.innerHTML="Follow"
+        count=0
+    }
 })
-unfollow.addEventListener("click",function(){
-    isstatus.innerHTML = "Follow Mountains"
-    isstatus.style.color= "Red"
-})
+// unfollow.addEventListener("click",function(){
+//     isstatus.innerHTML = "Follow Mountains"
+//     isstatus.style.color= "Red"
+// })
